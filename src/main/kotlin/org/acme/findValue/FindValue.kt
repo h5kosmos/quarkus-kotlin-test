54e8ec6e48ed694d80d432e569890e2369821d3e
denis.avdeev <denis.2013work@gmail.com>
2021-02-06 22:20:08 +0300

    fix directory path, add logging filter

--- src/main/kotlin/org/acme/findValue/FindValue.kt
+++ src/main/kotlin/org/acme/findValue/FindValue.kt
@@ -1,18 +1,12 @@
 package org.acme.findValue
 
-import Config
+import org.acme.config.Config
 import java.io.File
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
-import javax.ws.rs.Consumes
-import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
 
 @ApplicationScoped
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 class FindValue (@Inject private val config: Config) {
-
 
     fun getAllFiles(): MutableSet<File> {
         val result: MutableSet<File> = mutableSetOf()
